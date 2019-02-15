267d7717154bc1e7a741c2780f42e833e388a041
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-16 03:53:35 +0800

    add gallery_procedure.sh

---
+++ gallery_procedure.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SRC_DIR=~/Downloads
+PORT_NUM=1234
+
+if [ $# -eq 2 ]; then
+	SRC_DIR=$1
+	PORT_NUM=$2
+fi
+
+
+apt-get install imagemagick  
+apt-get install graphicsmagick  
+apt-get install graphicsmagick-imagemagick-compat
+
+npm install
+rm -rf ./raw_photos
+rm -rf 
+
+cp -rf $SRC_DIR ./raw_photos
+
+./rename_process.sh raw_photos photos
+./identify_process.sh photos
+# make preview
+$ ./node_modules/.bin/epg-prep ./photos
+
+node ./app.js
