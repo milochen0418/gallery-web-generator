f3124ce3bfe43726a979d013ed4ab2280fcc44fc
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-16 15:34:59 +0000

    Add code to parsing command line args to start web server show the gallery in different subdir in ./photos/

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var app = express();
 var port =1234;
+var subdir='ALL';
 var Gallery = require('express-photo-gallery');
 
 var options = {
-  title: 'My Awesome Photo Gallery'
+  title: 'Awesome Photo Gallery from Google'
 };
 
-//app.use('/photos', Gallery('/home/joanmirochen/downloads/美女', options));
-app.use('/photos', Gallery('./photos', options));
 
-console.log('port number = ' + port);
+const args = require('minimist')(process.argv.slice(2));
+
+
+if ( typeof(args['port'])  != "undefined" ) {
+	port=args['port']
+	console.log('port is defined as ' + port);
+}
+
+if ( typeof(args['subdir'])  != "undefined" ) {
+	subdir=args['subdir']
+	console.log('subdir is defined as ' + subdir);
+}
+
+
+app.use('/', Gallery('./photos/'+subdir, options));
+
+console.log('port number = ' + port + ' to show gallery = ' + subdir);
 app.listen(port);
+
+// There some examples to start web server by command line here
+// $ node ./app.js --port=8866 --subdir=校花
+// $ node ./app.js
+// $ node ./app.js --port=5566
