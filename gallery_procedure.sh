1b5f205f7a5d6bdedc1de679a3cef7795e966c23
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-16 23:54:57 +0800

    improve start shell procedure with three args

--- gallery_procedure.sh
+++ gallery_procedure.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-SRC_DIR=~/Downloads
-PORT_NUM=1234
 
-if [ $# -eq 2 ]; then
-	SRC_DIR=$1
-	PORT_NUM=$2
+PORT_NUM=1234
+SRC_DIR=~/Downloads
+SUB_DIR=ALL
+if [ $# -gt 0 ]; then
+	PORT_NUM=$1
+fi
+
+if [ $# -gt 1 ]; then
+	SRC_DIR=$2
+fi
+
+if [ $# -gt 1 ]; then
+	SUB_DIR=$3
 fi
 
 
@@ -18,7 +26,21 @@ apt-get install tree
 npm install
 
 
-./create_a_gallery.sh $SRC_DIR ./photos
+# the following  method is just for single folder to show
+# ./create_a_gallery.sh $SRC_DIR ./photos
 
 
-node ./app.js
+# But we have a galleries set and each gallery for each subdir
+./create_a_galleries_set.sh $SRC_DIR ./photos
+
+
+node ./app.js --port=$PORT_NUM --subdir=$SUB_DIR
+
+# if you call 
+# $ ./gallery_procedure.sh 8866 ../downloads 校花
+# Then the program will refer ../downlaods and create a galleries set in ./photos
+# Thus any subdir in ./photos is gallery. and subdir is the name of that gallery.
+# After galleries ready , then it will start the web by the following command
+# $ node ./app.js --port=8866 --subdir=校花
+# This command is mean you open gallery 校花 with port number 8866
+# So you can open browser to open http://yourhost:8866/  to see 校花 gallery
