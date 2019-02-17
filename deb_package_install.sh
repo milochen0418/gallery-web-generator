bef70e9a2667de930ca7852432805f2cd7008992
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-17 11:22:53 +0000

    fix bug

--- deb_package_install.sh
+++ deb_package_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-apt-get install imagemagick  
-apt-get install graphicsmagick  
-apt-get install graphicsmagick-imagemagick-compat
+#apt-get install imagemagick  
+#apt-get install graphicsmagick  
+apt-get install graphicsmagick-imagemagick-compat graphicsmagick
+
+
+#apt-get install imagemagick  
+#apt-get install graphicsmagick  
+#apt-get install graphicsmagick-imagemagick-compat
+
 apt-get install tree
