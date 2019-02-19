9e4f3945e268fe8c08e405118c67cdbb5f1db5f4
Milo Chen <milo_chen@mail2000.com.tw>
2019-02-19 09:58:40 +0000

    change rename_process.sh. so it is compatible now only google download but also any image list

--- rename_process.sh
+++ rename_process.sh
@@ -12,8 +12,12 @@ rm -rf $COPYTO_DIR
 rm -rf $COPY_DIR
 cp -rf $RAW_DIR $COPY_DIR
 mkdir -p $COPYTO_DIR
+
+CNT=0
 while read i; do
-	cmd=$(printf "mv '$COPY_DIR/%s' '$COPYTO_DIR/%sjpg'"  "$i"  "$(echo $i | awk '{print $1}')" )
+	CNT=$((CNT + 1))
+	#cmd=$(printf "mv '$COPY_DIR/%s' '$COPYTO_DIR/%sjpg'"  "$i"  "$(echo $i | awk '{print $1}')" )
+	cmd=$(printf "mv '$COPY_DIR/%s' '$COPYTO_DIR/%s.jpg'"  "$i"  "$CNT" )
 	echo $cmd
 	eval "$cmd"
 done < <(ls $COPY_DIR)
